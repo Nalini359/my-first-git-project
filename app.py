c2bf667e5d0da4ff063452b9b8c72636423e15a4
Nalini359 <nalinishoz@gmail.com>
2025-08-18 11:35:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """
-    <h1>Hello from Jenkins Python job!</h1>
-    <p>Jenkins pipeline automation is working perfectly!</p>
-    <p>Learning basics of DevOps</p>
-    """
+    return "Hello from Jenkins Python job!<br>Jenkins pipeline automation is working perfectly!<br>Learning basics of devops"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-print("Hello from Jenkins Python job!")
-print("Jenkins pipeline automation is working perfectly!")
-print("Learning basics of devops")
-
-
-
