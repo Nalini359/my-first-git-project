025deb4761e0aa25a3172e69a880693de8cf893e
Nalini359 <nalinishoz@gmail.com>
2025-08-18 11:22:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+    <h1>Hello from Jenkins Python job!</h1>
+    <p>Jenkins pipeline automation is working perfectly!</p>
+    <p>Learning basics of DevOps</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 print("Hello from Jenkins Python job!")
 print("Jenkins pipeline automation is working perfectly!")
 print("Learning basics of devops")
